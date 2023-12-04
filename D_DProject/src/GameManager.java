6afac696e31d85a8eb8b6b3f0f62ba12230df5ac
ocramgit <marstrabalhar@gmail.com>
2023-12-04 17:52:14 +0000

    gamemanager update

--- D_DProject/src/GameManager.java
+++ D_DProject/src/GameManager.java
@@ -14,16 +14,44 @@ public class GameManager {
         monsterArrayList = new ArrayList<>();
         character = playerSelectCharacter();
         addCharactersToList();
-        //addMonstersToList();
+        addMonstersToList();
+    }
+
+    private void addMonstersToList() {
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.SLIME));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.BABYSPIDERS));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.GOBLIN));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.SKELETON));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.ZOMBIE));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.SPECTRE));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.WEREWOLF));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.GHOST));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.WITCH));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.VAMPIRE));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.MUMMY));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.CREEPER));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.HARPY));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.CENTAUR));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.MINOTAUR));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.CYCLOPS));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.BASILISK));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.GRIFFIN));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.CHIMERA));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.MEDUSA));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.WYVERN));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.PHOENIX));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.KRAKEN));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.BEHEMOTH));
+        monsterArrayList.add(MonsterFactory.createMonster(MonsterType.DRAGON));
     }
 
     public Character playerSelectCharacter() {
         int count = 0;
         GameMessage.getPlayerSelectCharacterMenu();
         // TODO: 04/12/2023 loop entre character para apresentar no menu
-        /*for (Character character : characters) {
-            System.out.println(count++ + character.getName());
-        }*/
+        for (Character character : characters) {
+            System.out.println(count++ + " | "+character.getName());
+        }
         System.out.print("Select character:");
         return switch (sc.nextInt()) {
             case 1 -> new Character(CharacterType.KNIGHT);
--- D_DProject/src/Main.java
+++ D_DProject/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-<<<<<<< HEAD
-=======
     public static void main(String[] args) {
+
     }
->>>>>>> f57d1c5 (added some things)
 }
