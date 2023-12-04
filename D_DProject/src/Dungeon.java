f6698e1810082755e4b2e92d3c98ce88e498d3e7
ocramgit <marstrabalhar@gmail.com>
2023-12-04 15:47:02 +0000

    added some things

--- D_DProject/src/Dungeon.java
+++ D_DProject/src/Dungeon.java
@@ -12,17 +12,17 @@ public class Dungeon {
     private Character playerCharacter;
 
     public Dungeon(Player player) {
-        this.playerCharacter = player.getSelectedCharacter();
+        this.playerCharacter = player.getSELECTED_CHARACTER();
     }
 
     //Init dungeon with level 1 monsters
-    public void startDungeon(ArrayList<Monster> monster_level) {
+    /*public void startDungeon(ArrayList<Monster> monster_level) {
         for (Monster monster : monster_level_one) {
-            while (!monster.isDead) {
+            while (!monster.isAlive) {
                 playerCharacter.attack(monster);
             }
         }
-    }
+    }*/
 
     private void randomizeMonsters() {
         int random = (int) (Math.random() * 6) + 1;
--- D_DProject/src/GameManager.java
+++ D_DProject/src/GameManager.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameManager {
+
+    private ArrayList<Monster> monsterArrayList;
+    private Character character;
+    private Scanner sc;
+    private GameMessage gameMessage;
+
+    public GameManager() {
+        gameMessage = new GameMessage();
+        sc = new Scanner(System.in);
+        monsterArrayList = new ArrayList<>();
+        character = playerSelectCharacter();
+    }
+
+    public Character playerSelectCharacter() {
+        gameMessage.getPlayerSelectCharacterMenu();
+        System.out.print("Select character:");
+        return switch (sc.nextInt()) {
+            case 1 -> CharacterType.KNIGHT;
+            case 2 -> CharacterType.SORCERER;
+            case 3 -> CharacterType.BARD;
+            case 4 -> CharacterType.ASSASSIN;
+            default -> throw new IllegalStateException("Unexpected value.");
+        };
+    }
 }
