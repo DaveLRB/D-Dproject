ace4e246e8a3828418a0a1e579c34d98e59c6e67
ocramgit <marstrabalhar@gmail.com>
2023-12-04 10:54:33 +0000

    first structure of dungeon´

--- D_DProject/src/Dungeon.java
+++ D_DProject/src/Dungeon.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Dungeon {
+
+    private Character playerCharacter;
+
+    public Dungeon(Player player) {
+        // TODO: check getCharacter Method in Player Class
+        this.playerCharacter = player.getSelectedCharacter();
+    }
+
+    //Init dungeon with level 1 monsters
+    public void startDungeon(ArrayList<Monster> monsters) {
+
+    }
 }
