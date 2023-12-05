e41b082ebdf01eeed64806936f9b2e12a92085c7
ocramgit <marstrabalhar@gmail.com>
2023-12-05 11:53:01 +0000

    changed gamemanager methods to dungeon responsability

--- D_DProject/src/Dungeon.java
+++ D_DProject/src/Dungeon.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Dungeon {
@@ -15,16 +14,39 @@ public class Dungeon {
         this.playerCharacter = player.getSELECTED_CHARACTER();
     }
 
-    //Init dungeon with level 1 monsters
-    /*public void startDungeon(ArrayList<Monster> monster_level) {
-        for (Monster monster : monster_level_one) {
-            while (!monster.isAlive) {
-                playerCharacter.attack(monster);
-            }
-        }
-    }*/
+    //Following the level, this method creates 5 monsters.
+    private void addMonstersByLevel(int level) {
+        switch (level) {
+            case 1 ->
+                    createMonsters(MonsterType.SLIME, MonsterType.BABYSPIDERS, MonsterType.GOBLIN, MonsterType.SKELETON, MonsterType.ZOMBIE);
+            case 2 ->
+                    createMonsters(MonsterType.SPECTRE, MonsterType.WEREWOLF, MonsterType.GHOST, MonsterType.WITCH, MonsterType.VAMPIRE);
+            case 3 ->
+                    createMonsters(MonsterType.MUMMY, MonsterType.CREEPER, MonsterType.HARPY, MonsterType.CENTAUR, MonsterType.MINOTAUR);
+            case 4 ->
+                    createMonsters(MonsterType.CYCLOPS, MonsterType.BASILISK, MonsterType.GRIFFIN, MonsterType.CHIMERA, MonsterType.MEDUSA);
+            case 5 ->
+                    createMonsters(MonsterType.WYVERN, MonsterType.PHOENIX, MonsterType.KRAKEN, MonsterType.BEHEMOTH, MonsterType.DRAGON);
+            default -> throw new IllegalStateException("Unexpected value: " + level);
+        };
+    }
+
+    //Creating and adding monsters to the list following your type
+    private void createMonsters(MonsterType monster, MonsterType monster1, MonsterType monster2, MonsterType monster3, MonsterType monster4) {
+        clearMonsterList();
+        monsters.add(MonsterFactory.createMonster(monster));
+        monsters.add(MonsterFactory.createMonster(monster1));
+        monsters.add(MonsterFactory.createMonster(monster2));
+        monsters.add(MonsterFactory.createMonster(monster3));
+        monsters.add(MonsterFactory.createMonster(monster4));
+    }
 
     private void randomizeMonsters() {
         int random = (int) (Math.random() * 6) + 1;
+    }
+
+    //Remove all elements from monsterArray, generally to start a new level on createMonsters method
+    private void clearMonsterList() {
+        monsters.clear();
     }
 }
