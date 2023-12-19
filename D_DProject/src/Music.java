645edd06ed411cf08c9db739e403bf6cc57ab35d
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-12-19 09:09:47 +0000

    added a few music audios to test out

--- D_DProject/src/Music.java
+++ D_DProject/src/Music.java
@@ -11,6 +11,76 @@ public class Music {
         start.play();
      */
 
+    public void griffinNeutral2 (){
+        Music start = new Music("featherinstorm.wav");
+        start.play();
+    }
+
+    public void griffinNeutral3 (){
+        Music start = new Music("griffinsscreech.wav");
+        start.play();
+    }
+
+    public void griffinNeutral4 (){
+        Music start = new Music("gustofwind.wav");
+        start.play();
+    }
+
+    public void griffinNeutral5 (){
+        Music start = new Music("afeatheredsymphony.wav");
+        start.play();
+    }
+
+    public void griffinAnger1 (){
+        Music start = new Music("griffinsoars.wav");
+        start.play();
+    }
+
+    public void griffinAnger2 (){
+        Music start = new Music("feelthemight.wav");
+        start.play();
+    }
+
+    public void griffinAnger3 (){
+        Music start = new Music("skyroaring.wav");
+        start.play();
+    }
+
+    public void griffinAnger4 (){
+        Music start = new Music("talongust.wav");
+        start.play();
+    }
+
+    public void griffinAnger5 (){
+        Music start = new Music("featherfrenzy.wav");
+        start.play();
+    }
+
+    public void griffinSeduce1(){
+        Music start = new Music("aerialonslaugth.wav");
+        start.play();
+    }
+
+    public void griffinSeduce2(){
+        Music start = new Music(".wav");
+        start.play();
+    }
+
+    public void griffinSeduce3(){
+        Music start = new Music("stormthatsweeps.wav");
+        start.play();
+    }
+
+    public void griffinSeduce4(){
+        Music start = new Music("lovesoarshigh.wav");
+        start.play();
+    }
+
+    public void griffinSeduce5(){
+        Music start = new Music("inthefeatheredembrace.wav");
+        start.play();
+    }
+
     public Music(String musicFile) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream(musicFile));
